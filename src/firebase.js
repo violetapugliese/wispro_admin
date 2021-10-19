d44d4a4ec0c6b6a331b624de647fbbc252f76174
Violeta Pugliese <violeta.pugliese@gmail.com>
2021-10-19 02:45:21 -0300

    firebase hosting

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_APIKEY,
-  authDomain: process.env.FIREBASE_AUTHDOMAIN,
-  projectId: process.env.FIREBASE_PROJECTID,
-  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
-  appId: process.env.FIREBASE_APPID
+  apiKey: "AIzaSyCe-qKW6Mu8BEGRODciMrWmuVeIfwGLme4",
+  authDomain: "wispro-admin.firebaseapp.com",
+  databaseURL: "https://wispro-admin-default-rtdb.firebaseio.com",
+  projectId: "wispro-admin",
+  storageBucket: "wispro-admin.appspot.com",
+  messagingSenderId: "735595694556",
+  appId: "1:735595694556:web:4dbd9d9257b7604df96719"
 };
 
 const app = initializeApp(firebaseConfig);
