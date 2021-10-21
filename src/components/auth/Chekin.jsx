27aa2b42433057101545c4be3c80ff210bc91ae6
Violeta Pugliese <violeta.pugliese@gmail.com>
2021-10-21 01:41:12 -0300

    add terms and Conditions

--- src/components/auth/Chekin.jsx
+++ src/components/auth/Chekin.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { useForm } from 'react-hook-form'
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Modal} from "react-bootstrap";
 import {
     createUserWithEmailAndPassword,
     onAuthStateChanged,
@@ -16,6 +16,7 @@ const Login = () => {
     const [registerName, setRegisterName] = useState("");
     const [registerEmail, setRegisterEmail] = useState("");
     const [registerPassword, setRegisterPassword] = useState("");
+    const [registerCheckbox, setRegisterCheckbox] = useState();
 
     const [user, setUser] = useState({});
 
@@ -40,6 +41,12 @@ const Login = () => {
     const createUser = async () => {
         await addDoc(collection(db, "users"), { name: registerName, email: registerEmail })
     }
+
+    // Modal
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
 
     return (
         <>
@@ -71,15 +78,34 @@ const Login = () => {
                     <Form.Control
                         type="password"
                         className="rounded-pill"
-                        {...register('newpassword', { required: true})}
+                        {...register('newpassword', { required: true })}
                         placeholder="Clave"
                         onChange={(event) => { setRegisterPassword(event.target.value); }} />
                     {errors.newpassword && <span>Ingrese una clave de al menos 6 caracteres</span>}
                 </Form.Group>
-                <Form.Group className="mb-3 w-100 ps-2 d-flex" controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="" />
-                    <a href="" className="d-flex ps-2">Aceptar términos y condiciones</a>
+                <Form.Group className="mb-3 w-100 ps-2" controlId="formBasicCheckbox">
+                    <div className="d-flex">
+                        <Form.Check
+                            type="checkbox"
+                            label=""
+                            {...register('checkbox', { required: true })}
+                            placeholder="Clave"
+                            onChange={(event) => { setRegisterCheckbox(event.target.value); }} />
+                        <button className="btn text-light p-1 text-decoration-underline" onClick={handleShow}>Aceptar términos y condiciones</button>
+                    </div>
+                    {errors.checkbox && <span>Acepte terminos y condiciones</span>}
                 </Form.Group>
+                <Modal show={show} onHide={handleClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>Términos y condiciones</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>Estos son los términos y condiciones que aceptarás al registrate.</Modal.Body>
+                    <Modal.Footer>
+                        <Button className="roundend-pill" variant="secondary" onClick={handleClose}>
+                            Entendido
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
                 <Button type="submit" variant="dark" className="btn-auth rounded-pill">
                     Submit
                 </Button>
